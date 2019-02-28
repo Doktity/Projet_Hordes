e5256aa5dcff0bd6eece16615aa5cbb6888350ec
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-02-28 10:51:05 +0100

    Update carte.h

--- carte.h
+++ carte.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define T 12 //Nombres de cases de la carte
+#define T 13 //Nombres de cases de la carte
 
+void creer_carte(int * map[T*T);
+                           
+                           
