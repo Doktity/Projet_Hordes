d58375974850747960bb64e703105385b5174533
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-03-01 12:00:33 +0100

    Add files via upload

---
+++ objet.c
@@ -0,0 +1,18 @@
+#include "objet.h"
+
+objet_t recup_fichier(FILE * fic){
+
+}
+
+objet_t tab_categorie(objet_t tab[],char * categorie){
+
+}
+
+objet_t trouve_objet(objet_t * p,char * nom,char * categorie){
+
+}
+
+int main(){
+
+  return 0;
+}
