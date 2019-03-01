cf3381e3d0413ad306df342cc05bb7ce54c0d4b4
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-03-01 11:15:40 +0100

    Update objet.h

--- objet.h
+++ objet.h
@@ -9,6 +9,6 @@ typedef struct objet_s{
 
 objet_t recup_fichier(FILE * fic); // Cette fonction récupere les objets qui existe dans un fichier et les stockes dans un tableau
 
-objet_t tab_categorie(objet_t tab[],char * categorie)//Cette fonction retourne un tableau contenant les objets d'une catégorie
+objet_t tab_categorie(objet_t tab[],char * categorie);//Cette fonction retourne un tableau contenant les objets d'une catégorie
 
 objet_t trouve_objet(objet_t * p,char * nom,char * categorie);//Cette fonction retourne un pointeur sur un objet selon son nom et sa catégorie
