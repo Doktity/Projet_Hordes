3da8652cca1cf56706d26d5510da300e969bb4f8
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-03-06 14:47:48 +0100

    Update objet.c

--- objet.c
+++ objet.c
@@ -1,12 +1,12 @@
 #include "objet.h"
 
-objet_t recup_fichier(){
+objet_t * recup_fichier(){
   FILE * fic=NULL;
 	objet_t tab[nb_obj]; //Tableau qui stockera les valeurs contenues dans le fichier
 	int i=0;
 	if(fopen("liste_objet.txt","r")==NULL){ //Test pour savoir si le fichier est présent
 		printf("Erreur lors de l'ouverture du fichier liste_objet\n");
-		return NULL;
+		return 0;
 	}
 	else fic=fopen("liste_objet.txt","r");
 	fscanf(fic,"%s:%s:%s:%i",&tab[i].nom_obj,&tab[i].categorie,&tab[i].description,&tab[i].influ_pa);
