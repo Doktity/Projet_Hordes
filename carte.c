dba36518807cab2d06816105a49fd3e1b30c88a0
NomeUn <48085180+NomeUn@users.noreply.github.com>
2019-03-08 16:31:31 +0100

    Update carte.c

--- carte.c
+++ carte.c
@@ -39,4 +39,33 @@ void afficher_carte(t_mat * map){
       }
 }
 
+void action(int posx, int posy, joueur_t joueur,t_mat map){
+      int i = 1;
+      printf("quels actions voulez vous faire ?\n");
+      if (!posx) {
+            printf("%i:vous déplacer à gauche\n",i);
+            i++;
+      }
+      if (!(posx==map->nbc-1)) {
+            printf("%i:vous déplacer à droite\n",i);
+            i++;
+      }
+      if (!posy) {
+            printf("%i:vous déplacer en haut\n",i);
+            i++;
+      }
+      if (!(posy==map->nbl-1)) {
+            printf("%i:vous déplacer en bas\n",i);
+            i++;
+      }
+      printf("%i:fouiller la zone\n",i);
+      if (map->mat[posx][posy].nb_zombie) {
+            printf("%i:attaquer un zombie\n", i);
+            i++;
+      }
+      if (!liste_vide(map->mat->liste_objet)) {
+            printf("%i:ramasser les objets\n",i);
+      }
 
+
+}
