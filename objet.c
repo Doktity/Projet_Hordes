ddc45b78911052979ea12704c0d6579f6387df4a
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-03-08 16:28:53 +0100

    Update objet.c

--- objet.c
+++ objet.c
@@ -1,7 +1,7 @@
 #include "objet.h"
 
-objet_t * ec; //L'élement courant de la liste
-objet_t * drapeau;
+liste_objet_t * ec; //L'élement courant de la liste
+liste_objet_t * drapeau;
 
 void init_liste(void){
   drapeau=malloc(sizeof(objet_t));
@@ -50,36 +50,65 @@ void valeur_elt(objet_t *v){
 
 void modif_elt(objet_t v){
   if(!hors_liste()){
-      ec->valeur=v;
+      ec->val=v;
   }
 }
 
 void oter_elt(void){
-
+	if(!hors_liste()){
+		liste_objet_t * mem;
+		ec->succ->pred=ec->pred;
+		ec->pred->succ=ec->succ;
+		mem=ec->succ;
+		free(ec);
+		ec->pred=mem;
+		mem->succ=ec;
+	}
 }
 
 void ajout_droit(objet_t v){
-
+	if(liste_vide()||!hors_liste()){
+		liste_objet_t * nouv;
+		nouv=malloc(sizeof(liste_objet_t));
+		nouv->val=v;
+		nouv->pred=ec;
+		nouv->succ=ec->succ;
+		ec->succ->pred=nouv;
+		ec->succ=nouv;
+		ec=nouv;
+	}
 }
 
 void ajout_gauche(objet_t v){
-
+	  if(liste_vide()||!hors_liste()){
+    liste_objet_t * nouv;
+    nouv=malloc(sizeof(liste_objet_t));
+    nouv->val=v;
+    nouv->pred=ec;
+    nouv->succ=ec->pred;
+    ec->pred=nouv;
+    ec->succ=nouv;
+    ec=nouv;
+  }
 }
 
 objet_t * recup_fichier(){
 	FILE * fic=NULL;
-	objet_t tab[nb_obj]; //Tableau qui stockera les valeurs contenues dans le fichier
+	objet_t mem;
+	init_liste();
+	en_tete();
 	int i=0;
 	if(fopen("liste_objet.txt","r")==NULL){ //Test pour savoir si le fichier est présent
 		printf("Erreur lors de l'ouverture du fichier liste_objet\n");
-		return 0;
+		return;
 	}
 	else fic=fopen("liste_objet.txt","r");
-	fscanf(fic,"%s:%s:%s:%i",tab[i]->nom_obj,tab[i]->categorie,tab[i]->description,&tab[i]->influ_pa);
+	fscanf(fic,"%s:%s:%[^\n]:%i",mem.nom_obj,mem.nom_obj,mem.description,&mem.influ_pa);
+	ajou_droit(mem);
 	while(!feof(fic)){
 		i++;
-		fscanf(fic,"%s:%s:%s:%i",tab[i]->nom_obj,tab[i]->categorie,tab[i]->description,&tab[i]->influ_pa);
+		fscanf(fic,"%s:%s:%[^\n]:%i",mem.nom_obj,mem.nom_obj,mem.description,&mem.influ_pa);
+		ajout_droit(mem);
 	}
 	fclose(fic);
-	return *tab;
 }
