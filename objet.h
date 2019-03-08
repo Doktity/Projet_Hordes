74473abaf3d161e0ce1db5423bc90687aaea789f
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-03-08 15:01:09 +0100

    Update objet.h

--- objet.h
+++ objet.h
@@ -10,4 +10,36 @@ typedef struct objet_s{
   int influ_pa;
 }objet_t;
 
+typedef struct liste_objet_s{
+  objet_t val;
+  objet_t * pred;
+  objet_t *succ;
+}
+
+//Primitives d'accès à la liste
+
+void init_liste(void);
+
+int liste_vide(void);
+
+int hors_liste(void);
+
+void en_tete(void);
+
+void en_queue(void);
+
+void precedent(void);
+
+void suivant(void);
+
+void valeur_elt(objet_t*);
+
+void modif_elt(objet_t);
+
+void oter_elt(void);
+
+void ajout_droit(objet_t);
+
+void ajout_gauche(objet_t);
+
 objet_t * recup_fichier(); // Cette fonction récupere les objets qui existe dans un fichier et les stockes dans un tableau
