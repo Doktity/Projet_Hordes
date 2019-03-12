8656b59926a0990a2dfe8cde458dc86dce447db7
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-12 15:04:41 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,16 +1,24 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <pthread.h>
 
-//#include "joueur.h"
+#include "joueur.h"
+#include "objet.h"
 #include "carte.h"
 
-#define DIX 600
+#define DIX 6
+#define NB_ZOMBIE_MIN 10
+#define NB_ZOMBIE_MAX 250
 
 
 typedef struct {
   int nb_tour;
   time_t temps;
 }tour_t;
+
+#endif
 
