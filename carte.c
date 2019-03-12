79f49c5e6dbdd27ec200dd35b8d3a77f807696c7
NomeUn <48085180+NomeUn@users.noreply.github.com>
2019-03-12 13:47:35 +0100

    Update carte.c

--- carte.c
+++ carte.c
@@ -36,23 +36,15 @@ void afficher_carte(t_mat * map){
       }
 }
 
-void afficher_carte(t_mat * map){
-      for (int i = 0; i < map->nbl; i++) {
-            for (int j = 0; j < map->nbc; j++) {
-                  if (map->mat[i][j].nb_joueur) {
-                        printf("%i",map->mat[i][j].nb_joueur);
-                  }
-                  else{
-                        printf(" ");
-                  }
-                  switch (map->mat[i][j].etat) {
-                        case ville:printf("v ");break;
-                        case non_explore:printf("ø ");break;
-                        case explore_neutre:printf("o ");break;
-                        case explore_zombie:printf("z%i",map->mat[i][j].nb_zombie);break;
-                        default:printf("?");
-                  }
-            }
-            printf("\n\n");
+t_mat * calculer_pos_zombie(t_mat * map, int nb_jour){
+      int nb_zombie_today;
+      //demander à la prof de maths comment récupérer coos d'un point d'une fonction logarithme
+      for (int i = 0; i < nb_zombie_today; i++) {
+            int x,y;
+            do {
+                  x = nb_aleatoire(map->nbl);
+                  y = nb_aleatoire(map->nbc);
+            } while(x == (map->nbl)/2 && y == (map->nbc)/2);//tant que c'est égal à la position de la ville
+            map->mat[i][j].nb_zombie ++;
       }
 }
