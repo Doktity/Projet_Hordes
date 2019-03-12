3943fc97b7f51868878ac1e3e615440f07f8fe0b
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-12 15:06:16 +0100

    Update carte.h

--- carte.h
+++ carte.h
@@ -3,15 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "matrice.h"
 
+#include "matrice.h"
 
 #define T  13//taille des côtés de la carte
 
 t_mat * creer_carte();
 
 void afficher_carte(t_mat * map);
-
-t_mat * calculer_pos_zombie(t_mat * map, int nb_jour);
 
 #endif
