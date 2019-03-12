bbbd976dada33e550cfa1198b7a84330bbc98787
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-03-12 12:01:32 +0100

    Update objet.h

--- objet.h
+++ objet.h
@@ -14,38 +14,45 @@ typedef struct liste_objet_s{
   objet_t val;
   struct liste_objet_s * pred;
   struct liste_objet_s * pred;
+    struct liste_objet_s * ec;
+  struct liste_objet_s * drapeau;
 }liste_objet_t;
 
 /**
 *Primitives d'accès à la liste
 */
-void init_liste(void);
+void init_liste(liste_objet_t*);
 
-int liste_vide(void);
+int liste_vide(liste_objet_t*);
 
-int hors_liste(void);
+int hors_liste(liste_objet_t*);
 
-void en_tete(void);
+void en_tete(liste_objet_t*);
 
-void en_queue(void);
+void en_queue(liste_objet_t*);
 
-void precedent(void);
+void precedent(liste_objet_t*);
 
-void suivant(void);
+void suivant(liste_objet_t*);
 
-void valeur_elt(objet_t*);
+void valeur_elt(objet_t*,liste_objet_t*);
 
-void modif_elt(objet_t);
+void modif_elt(objet_t,liste_objet_t*);
 
-void oter_elt(void);
+void oter_elt(liste_objet_t*);
 
-void ajout_droit(objet_t);
+void ajout_droit(objet_t,liste_objet_t*);
 
-void ajout_gauche(objet_t);
+void ajout_gauche(objet_t,liste_objet_t*);
 
 /**
 * Cette fonction récupere les objets qui existe dans un fichier et les stockes dans un tableau
 */
-void creer_liste(void); 
+void creer_liste(liste_objet_t *);
 
-void affiche_liste(void);
+/**
+* Affiche_liste affiche la liste complète des objets
+* Pour chaque objet on indique son nom, sa catégorie, sa description et son influence sur les points d'actions
+*/
+void affiche_liste(liste_objet_t*);
+
