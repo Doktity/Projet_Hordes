e2852e5b299cb5fa8f3f2e2d0631a585bc0df280
NomeUn <48085180+NomeUn@users.noreply.github.com>
2019-03-12 14:50:46 +0100

    Update matrice.c

--- matrice.c
+++ matrice.c
@@ -1,10 +1,10 @@
 #include "matrice.h"
 
-t_mat * alouer_matrice(int nbl,int nbc,void * type_case){//nbl:nombreligne, nbc:nombrecolonne
+t_mat * alouer_matrice(int nbl,int nbc){//nbl:nombreligne, nbc:nombrecolonne
       t_mat * new=malloc(sizeof(t_mat));
-      new->mat = malloc(sizeof(type_case)*nbl);
+      new->mat = malloc(sizeof(case_t)*nbl);
       for (int i = 0; i < nbl; i++) {
-            new->mat[i]=malloc(sizeof(type_case)*nbc);
+            new->mat[i]=malloc(sizeof(case_t)*nbc);
       }
       new->nbl = nbl;
       new->nbc = nbc;
