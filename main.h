57f1ffdafaea06081247d42d12823020667d487e
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-12 10:59:57 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-#include "joueur.h"
-/*#include "carte.h"*/
+#include <pthread.h>
 
-#define DIX 6
+//#include "joueur.h"
+#include "carte.h"
 
-typedef struct objet_s objet_t;
+#define DIX 600
+
 
 typedef struct {
   int nb_tour;
   time_t temps;
 }tour_t;
+
