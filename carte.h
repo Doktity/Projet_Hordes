6403e1656d0f493154c49c91f9cdaf2ba8a592bf
NomeUn <48085180+NomeUn@users.noreply.github.com>
2019-03-14 14:17:55 +0100

    Update carte.h

--- carte.h
+++ carte.h
@@ -1,10 +1,12 @@
 #ifndef _CARTE_H_
 #define _CARTE_H_
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "boite_outil.h"
 #include "matrice.h"
+
 
 #define T  13//taille des côtés de la carte
 
@@ -12,4 +14,5 @@ t_mat * creer_carte();
 
 void afficher_carte(t_mat * map);
 
+t_mat * calculer_pos_zombie(t_mat * map, int nb_jour);
 #endif
