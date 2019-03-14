30e36a0dfefe0503200a7b455815df47126c988e
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-14 16:11:51 +0100

    Update action.h

--- action.h
+++ action.h
@@ -4,4 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "joueur.h"
+#include "objet.h"
+#include "carte.h"
+
+
+void dehors(int posx, int posy, joueur_t * joueur,t_mat map);
+void deposer_objet(liste_objet_t * l, joueur_t * joueur);
+void prendre_objet (liste_objet_t * l, joueur_t * joueur);
+
 #endif
