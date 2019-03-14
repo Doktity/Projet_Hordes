3039b7a3ba44c9efb4aeffa172bf93d901983ccc
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-14 16:12:44 +0100

    Update joueur.c

--- joueur.c
+++ joueur.c
@@ -3,7 +3,7 @@
 #include "joueur.h"
 
 
-joueur_t * Creer_Joueur(char * nom, int pa, objet_t inventaire[4], int statut[7]){
+joueur_t * Creer_Joueur(char * nom, int pa, objet_t inventaire[TAILLE_INVENTAIRE], int statut[NB_STATUT]){
 
   joueur_t * joueur = malloc(sizeof(joueur_t));
   int i;
@@ -19,36 +19,66 @@ joueur_t * Creer_Joueur(char * nom, int pa, objet_t inventaire[4], int statut[7]
   return joueur;
 }
 
+
 void Supprimer_Joueur(joueur_t * joueur){
   free(joueur);
   joueur = NULL;
 }
 
+
 int Est_Clair(joueur_t * joueur){
   return joueur->statut[0];
 }
+
 
 int Est_Soif(joueur_t * joueur){
   return joueur->statut[1];
 }
 
+
 int Est_Fatigue(joueur_t * joueur){
   return joueur->statut[2];
 }
+
 
 int Est_Blessure(joueur_t * joueur){
   return joueur->statut[3];
 }
 
+
 int Est_Drogue(joueur_t * joueur){
   return joueur->statut[4];
 }
+
 
 int Est_Rassasie(joueur_t * joueur){
   return joueur->statut[5];
 }
 
+
 int Est_Immunise(joueur_t * joueur){
   return joueur->statut[6];
 }
+
+
+void Afficher_inventaire(joueur_t * joueur){
+  int i;
+  for(i = 0; i<TAILLE_INVENTAIRE; i++){
+    if(inventaire[i] != NULL){
+      printf("Emplacement %d : %s\n", i+1, inventaire[i]->nom);
+    }
+  }
+}
+
+
+void  Trier_inventaire(joueur_t * joueur){
+  int i;
+  for(i=0; i<TAILLE_INVENTAIRE-1; i++){
+    if(joueur->inventaire[i] == NULL){
+      joueur->inventaire[i] = joueur->inventaire[i+1];
+      joueur->inventaire[i+1] = NULL;
+    }
+  }
+}
+
 
