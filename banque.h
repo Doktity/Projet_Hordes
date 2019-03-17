5a422b3c9579599eb6f93115e36ee79d5038d710
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-03-17 18:03:17 +0100

    Update banque.h

--- banque.h
+++ banque.h
@@ -1,11 +1,19 @@
 #include "objet.h"
 
-void banque(liste_objet_t*,joueur_t*);
+typedef struct banque_s{
+  liste_objet_t * nourriture;
+  liste_objet_t * drogue;
+  liste_objet_t * arme;
+  liste_objet_t *obj_construct;
+}banque_t;
 
-void affiche_banque(liste_objet_t *, joueur_t*);
 
-void ajouter_objet(liste_objet_t *,joueur_t *);
+void banque(banque_t*,joueur_t*);
 
-void retirer_objet(liste_objet_t *,joueur_t *);
+void affiche_banque(banque_t *, joueur_t*);
 
-void init_banque(liste_objet_t *);
+void ajouter_objet_banque(banque_t *,joueur_t *);
+
+void retirer_objet_banque(banque *,joueur_t *);
+
+void init_banque(banque_t *);
