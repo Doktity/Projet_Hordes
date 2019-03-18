0df5a3094ab49c82eabe8585571a501b3a57ad7a
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-18 09:10:30 +0100

    Create maison.h

---
+++ maison.h
@@ -0,0 +1,14 @@
+#ifndef _MAISON_H_
+#def _MAISON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "joueur.h"
+#include "action.h"
+
+void maison(t_joueur * joueur);
+
+void ameliorer_maison(t_joueur * joueur);
+
+#endif
