d31c24864d5785bf4f99357c1fc980313e40f884
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-18 16:21:59 +0100

    Update carte.c

--- carte.c
+++ carte.c
@@ -2,7 +2,7 @@
 
 t_mat * creer_carte(){
       //initialisation de la carte
-      t_mat * map = allouer_matrice(T,T);
+      t_mat * map = alouer_matrice(T,T);
       for (int i = 0; i < map->nbl; i++) {
             for (int j = 0; j < map->nbc; j++) {
                   map->mat[i][j].etat = non_explore;
@@ -37,7 +37,7 @@ void afficher_carte(t_mat * map){
 
 t_mat * calculer_pos_zombie(t_mat * map, int nb_jour, int nb_zombie_hier){
 
-      nb_zombie_today=(int)log(nb_jour)*10+nb_zombie_hier;
+      int nb_zombie_today=(int)(/*log*/ (nb_jour)*10+nb_zombie_hier);
 
       for (int i = 0; i < nb_zombie_today; i++) {
             int x,y;
@@ -47,38 +47,4 @@ t_mat * calculer_pos_zombie(t_mat * map, int nb_jour, int nb_zombie_hier){
             } while(x == (map->nbl)/2 && y == (map->nbc)/2);//tant que c'est égal à la position de la ville
             map->mat[x][y].nb_zombie ++;
       }
-}
-
-/*
-void action(int posx, int posy, joueur_t joueur,t_mat map){
-      int i = 1,choix;
-      printf("quels actions voulez vous faire ?\n");
-      if (posx) {
-            printf("%i:vous déplacer à gauche\n",i);
-            i++;
-      }
-      if (!(posx==map->nbc-1)) {
-            printf("%i:vous déplacer à droite\n",i);
-            i++;
-      }
-      if (posy) {
-            printf("%i:vous déplacer en haut\n",i);
-            i++;
-      }
-      if (!(posy==map->nbl-1)) {
-            printf("%i:vous déplacer en bas\n",i);
-            i++;
-      }
-      printf("%i:fouiller la zone\n",i);
-      if (map->mat[posx][posy].nb_zombie) {
-            printf("%i:attaquer un zombie\n", i);
-            i++;
-      }
-      if (!liste_vide(map->mat->liste_objet)) {
-            printf("%i:ramasser objets\n",i);
-      }
-      scanf("%i\n", &choix);
-      switch (choix) {
-            case :
-      }*/
 }
