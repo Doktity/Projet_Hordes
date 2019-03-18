7d105a3d973ed0fa11d5ebdd188dbe587af8d5aa
NomeUn <48085180+NomeUn@users.noreply.github.com>
2019-03-18 16:22:13 +0100

    Update carte.c

--- carte.c
+++ carte.c
@@ -2,7 +2,7 @@
 
 t_mat * creer_carte(){
       //initialisation de la carte
-      t_mat * map = alouer_matrice(T,T);
+      t_mat * map = allouer_matrice(T,T);
       for (int i = 0; i < map->nbl; i++) {
             for (int j = 0; j < map->nbc; j++) {
                   map->mat[i][j].etat = non_explore;
@@ -15,6 +15,7 @@ t_mat * creer_carte(){
 }
 
 void afficher_carte(t_mat * map){
+      system("clear");
       for (int i = 0; i < map->nbl; i++) {
             for (int j = 0; j < map->nbc; j++) {
                   if (map->mat[i][j].nb_joueur) {
@@ -37,7 +38,7 @@ void afficher_carte(t_mat * map){
 
 t_mat * calculer_pos_zombie(t_mat * map, int nb_jour, int nb_zombie_hier){
 
-      int nb_zombie_today=(int)(/*log*/ (nb_jour)*10+nb_zombie_hier);
+      int nb_zombie_today=(int)log(nb_jour)*10+nb_zombie_hier;
 
       for (int i = 0; i < nb_zombie_today; i++) {
             int x,y;
@@ -47,4 +48,68 @@ t_mat * calculer_pos_zombie(t_mat * map, int nb_jour, int nb_zombie_hier){
             } while(x == (map->nbl)/2 && y == (map->nbc)/2);//tant que c'est égal à la position de la ville
             map->mat[x][y].nb_zombie ++;
       }
+      return map;
+}send
+
+
+static void gauche(joueur_t joueur,t_mat * map) {
+      if () {
+
+      }
+}
+
+static void droite(joueur_t joueur,t_mat * map){
+      if () {
+
+      }
+}
+
+static void haut(joueur_t joueur,t_mat * map){
+
+}
+
+static void bas(joueur_t joueur,t_mat * map){
+
+}
+
+static void fouiller(joueur_t joueur,t_mat * map){
+
+}
+
+static void attaquer(joueur_t joueur,t_mat * map){
+
+}
+
+static void ramasser(joueur_t joueur,t_mat * map){
+
+}
+
+void action(int posx, int posy, joueur_t joueur, t_mat * map){
+      int choix=0;
+      while (choix!=9) {
+            printf("quels actions voulez vous faire ?\n");
+            printf("1:vous déplacer à gauche\n");
+            printf("2:vous déplacer à droite\n");
+            printf("3:vous déplacer en haut\n");
+            printf("4:vous déplacer en bas\n");
+            printf("5:fouiller la zone\n");
+            printf("6:attaquer un zombie\n");
+            printf("7:ramasser objets\n");
+            printf("8:afficher la carte\n");
+            printf("9:sortir de la carte\n");
+
+            scanf("%i\n", &choix);
+
+            switch (choix) {
+                  case 1:gauche(joueur,map);break;
+                  case 2:droite(joueur,map);break;
+                  case 3:haut(joueur,map);break;
+                  case 4:bas(joueur,map);break;
+                  case 5:fouiller(joueur,map);break;
+                  case 6:attaquer(joueur,map);break;
+                  case 7:ramasser(joueur,map);break;
+                  case 8:afficher_carte(map);break;
+                  case 9:printf("boom bye bye\n");break;
+                  default: printf("erreur de choix\n");
+            }
 }
