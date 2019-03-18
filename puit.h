7e313df70e3754ed10267120fbefdd9d14ad1fdc
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-18 12:04:26 +0100

    Create puit.h

---
+++ puit.h
@@ -0,0 +1,15 @@
+#ifndef _PUIT_H_
+#define _PUIT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "joueur.h"
+#include "objet.h"
+
+void puit(int * ration, joueur_t * joueur);
+
+void prendre_eau();
+
+#endif
