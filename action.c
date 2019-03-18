6c3d69b1095e74f9d8494f24d8dd29aafc220577
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-18 10:44:45 +0100

    Update action.c

--- action.c
+++ action.c
@@ -52,16 +52,26 @@ void deposer_objet(liste_objet_t * l, joueur_t * joueur){
 
 
 void prendre_objet (liste_objet_t * l, joueur_t * joueur){
-
-  
-
-}
-
-
-afficher_liste (liste_objet * l){
       int i;
+      char * nom_objet;
       
+      for(i = 0; i < TAILLE_INVENTAIRE && joueur->inventaire[i] != NULL; i++);
       
-          
+      if(i != TAILLE_INVENTAIRE){
+      
+            afficher_liste(l);
+
+            do
+            {
+                  printf("Donner le nom de l'objet que vous voulez prendre : ");
+                  scanf("%s", nom_objet);
+            }while(!est_present(l, nom_objet));
+
+            joueur->inventaire[i] = trouve_objet(l, nom_objet);
+            supprimer_objet(l, nom_objet);
+
+      }else{
+            printf("Vous n'avez pas de place dans votre inventaire !\n");
+      }
 }
 
