f850feee28c52e38986b58c918a315716b5799ef
NomeUn <48085180+NomeUn@users.noreply.github.com>
2019-03-20 10:04:44 +0100

    Update carte.h

--- carte.h
+++ carte.h
@@ -16,7 +16,7 @@ void afficher_carte(t_mat * map);
 
 t_mat * calculer_pos_zombie(t_mat * map, int nb_jour, int nb_zombie_hier);
 
-void action(int posx, int posy, joueur_t joueur,t_mat map);
+void action_carte(joueur_t * joueur,t_mat map);
 
 static void gauche(joueur_t joueur,t_mat * map);
 static void droite(joueur_t joueur,t_mat * map);
