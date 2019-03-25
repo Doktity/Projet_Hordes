976831eb39f5c27a471201a77873371e8bb929f3
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-25 16:30:46 +0100

    Update puit.c

--- puit.c
+++ puit.c
@@ -29,28 +29,31 @@ void puit (int * ration, joueur_t * joueur, liste_objet_t * liste){
 void prendre_eau (int * ration, joueur_t * joueur, liste_objet_t * liste){
   int i;
   char * nom = "eau";
-
-  if(!inventaire_vide(joueur)){
-
-    *joueur->inventaire[i] = trouver_objet(liste, nom);
+  for(i = 0; i < TAILLE_INVENTAIRE && joueur->inventaire[i] != NULL; i++);
+  if(i != TAILLE_INVENTAIRE){
+    joueur->inventaire[i] = trouver_objet(liste, nom);
     ration--;
-
   }else{
-    printf("Vous n'avez pas de place dans votre inventaire !\n");
+    printf("Vous n'avez pas la place dans votre inventaire !\n");
   }
 }
 
 
 void ajouter_eau (int * ration, joueur_t * joueur, liste_objet_t * liste){
-  int i;
+  int i, ajout = 0;
   char * nom = "eau";
 
-  for(i = 0; i < TAILLE_INVENTAIRE && !strcmp(joueur->inventaire[i]->nom_obj, nom); i++);
-  if(i != TAILLE_INVENTAIRE){
-    joueur->inventaire[i] = NULL;
-    ration++;
-    trier_inventaire(joueur);
-  }else{
-    printf("Vous n'avez pas de ration d'eau !\n");
+  for(i = 0; i < TAILLE_INVENTAIRE && joueur->inventaire[i] != NULL; i++){
+    if(strcmp(joueur->inventaire[i]->nom_obj, nom) == 0 && ajout == 0){
+      joueur->inventaire[i] = NULL;
+      ration++;
+      trier_inventaire(joueur);
+      ajout++;
+      printf("Ajout effectué.\n");
+    }
+  }
+
+  if(ajout == 0){
+    printf("Vous n'avez pas de ration d'eau dans votre inventaire !\n");
   }
 }
