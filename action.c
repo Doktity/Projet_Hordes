9f9009da740c7a5bfe07c5ce40d3685fe178b3d9
Doktity <38131269+Doktity@users.noreply.github.com>
2019-03-25 16:31:08 +0100

    Update action.c

--- action.c
+++ action.c
@@ -8,15 +8,18 @@ void deposer_objet(liste_objet_t * l, joueur_t * joueur){
   int num;
 
   afficher_inventaire(joueur);
+  if(!inventaire_vide(joueur)){
+      do{
+      printf("Donner le numéro de case de l'objet que vous voulez déposer (1,2,3,4): ");
+      scanf("%i", &num);
+      }while(num>4 || num<0);
 
-  do{
-    printf("Donner le numéro de case de l'objet que vous voulez déposer (1,2,3,4): ");
-    scanf("%i", &num);
-  }while(num>4 || num<0);
-
-  objet_ajout_droit(*joueur->inventaire[num-1], l);
-  joueur->inventaire[num-1] = NULL;
-  trier_inventaire(joueur);
+      objet_ajout_droit(*joueur->inventaire[num-1], l);
+      joueur->inventaire[num-1] = NULL;
+      trier_inventaire(joueur);
+  }else{
+        printf("Votre inventaire est vide !\n");
+  }
 
 }
 
@@ -26,22 +29,20 @@ void prendre_objet (liste_objet_t * l, joueur_t * joueur){
       char * nom_objet;
 
       for(i = 0; i < TAILLE_INVENTAIRE && joueur->inventaire[i] != NULL; i++);
-
       if(i != TAILLE_INVENTAIRE){
 
             objet_afficher_liste(l);
 
-            do
-            {
+            do{
                   printf("Donner le nom de l'objet que vous voulez prendre : ");
                   scanf("%s", nom_objet);
-            }while(!objet_est_present(l, nom_objet));
+            }while(!est_present_objet(l, nom_objet));
 
-            *joueur->inventaire[i] = trouver_objet(l, nom_objet);
-            objet_supprimer(l, nom_objet);
+            joueur->inventaire[i] = trouver_objet(l, nom_objet);
+            supprimer_objet(l, nom_objet);
 
       }else{
-            printf("Vous n'avez pas de place dans votre inventaire !\n");
+            printf("Vous n'avez pas la place dans votre inventaire !\n");
       }
 }
 
@@ -64,6 +65,3 @@ void utiliser_objet (joueur_t * joueur){
 int nb_aleatoire(int ecart){
       return(rand()%ecart);
 }
-
-      
-
