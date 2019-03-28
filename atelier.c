ad99fd83dadf294ab311816c875d080ebd127256
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-03-28 12:00:52 +0100

    Add files via upload

---
+++ atelier.c
@@ -0,0 +1,17 @@
+#include "atelier.h"
+
+void atelier(banque_t * b, joueur_t * j){
+    int choix;
+    do{
+        printf("\nAtelier\n-------\nQue voulez-vous faire ?\n");
+        printf("1 - Afficher les objets de l'inventaire\n")
+        printf("2 - Faire une transformation\n");
+        printf("3 - Quitter l'atelier\n");
+        printf("Votre choix:");
+        scanf("%d",&choix);
+
+        switch(choix){
+            
+        }
+
+}
