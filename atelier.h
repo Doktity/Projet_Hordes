4dd42bccfa26ad15909c566fee06653eefc4ca3c
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-04-02 16:01:31 +0200

    Update atelier.h

--- atelier.h
+++ atelier.h
@@ -3,10 +3,11 @@
 
 #include "banque.h"
 
-void atelier(banque_t *);
+
+void atelier(banque_t *, liste_objet_t *, joueur_t * j);
 
 objet_t * trouver_objet_vfinal(liste_objet_t *, char *);
 
-void transformation(banque_t *);
+void transformation(banque_t *, liste_objet_t *, joueur_t *);
 
 #endif
