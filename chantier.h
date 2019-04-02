4aed3d469340e461a8fde1fd975140fc9bc0a035
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-04-02 16:28:01 +0200

    Add files via upload

---
+++ chantier.h
@@ -0,0 +1,11 @@
+#include "banque.h"
+
+typedef struct chantier_s{
+  int defense;
+  int pa_requis;
+  objet_t * objet_1;
+  objet_t * objet_2;
+  int nb_obj1;
+  int nb_obj2;
+  int construit;
+}chantier_t;
