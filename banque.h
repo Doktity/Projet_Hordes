d47763279f1378ee9044e87f8410542df2c87146
Doktity <38131269+Doktity@users.noreply.github.com>
2019-04-09 01:37:51 +0200

    Update banque.h

--- banque.h
+++ banque.h
@@ -7,11 +7,11 @@ typedef struct banque_s{
   liste_objet_t * nourriture;
   liste_objet_t * drogue;
   liste_objet_t * arme;
-  liste_objet_t *obj_construct;
+  liste_objet_t * transformable;
+  liste_objet_t * materiau;
 }banque_t;
 
-
-void banque(int choix, banque_t *,joueur_t*);
+void banque(banque_t *,joueur_t*);
 
 void affiche_banque(banque_t *);
 
