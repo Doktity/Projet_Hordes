555f7fe8de481e59def108650a651fe60e740b11
Doktity <38131269+Doktity@users.noreply.github.com>
2019-04-09 01:36:53 +0200

    Update banque.c

--- banque.c
+++ banque.c
@@ -1,26 +1,32 @@
 #include "banque.h"
 
-void banque(int choix, banque_t * l, joueur_t * j){
-
-    switch(choix){
-        case 1: affiche_banque(l);
-                break;
-        case 2: if(!inventaire_vide(j)){
-                    ajouter_objet_banque(l,j);
-                }else{
-                    printf("Votre inventaire est vide !\n");
-                }
-                break;
-        case 3: retirer_objet_banque(l,j);
-                break;
-    }
-}
-
+void banque(banque_t * l, joueur_t * j){
+  int choix;
+    do{
+        printf("Bienvenue à la banque\n");
+        printf("Que voulez-vous faire?\n");
+        printf(" 1 - Afficher la liste des objets de la banque\n");
+        printf(" 2 - Ajouter un objet dans la banque\n");
+        printf(" 3 - Prendre un objet de la banque\n");
+        printf(" 4 - Quitter la banque\n");
+        printf("Votre choix:");
+        scanf("%d",&choix);
+        switch(choix){
+            case 1: affiche_banque(l); break;
+            case 2: ajouter_objet_banque(l,j); break;
+            case 3: retirer_objet_banque(l,j); break;
+            case 4:break;
+            default: printf("Erreur, vous devez entrer un chiffre entre 1 et 4\n");
+        }
+    }while(choix!=4);
+]
+    
 void init_banque(banque_t * b){
    objet_init_liste(&b->nourriture);
    objet_init_liste(&b->drogue);
    objet_init_liste(&b->arme);
-   objet_init_liste(&b->obj_construct);
+   objet_init_liste(&b->transformable);
+   objet_init_liste(&b->materiau);
 }
 
 void affiche_banque(banque_t * b){
@@ -30,8 +36,10 @@ void affiche_banque(banque_t * b){
     objet_afficher_liste(b->drogue);
     printf("Arme:\n");
     objet_afficher_liste(b->arme);
-    printf("Objet de construction:\n");
-    objet_afficher_liste(b->obj_construct);
+    printf("Objet transformable:\n");
+    objet_afficher_liste(b->transformable);
+    printf("Matériau:\n");
+    objet_afficher_liste(b->materiau);
 }
 
 void ajouter_objet_banque(banque_t * b, joueur_t * j){
@@ -40,13 +48,15 @@ void ajouter_objet_banque(banque_t * b, joueur_t * j){
     printf("1 - Nourriture\n");
     printf("2 - Drogue\n");
     printf("3 - Arme\n");
-    printf("4 - Objet de construction\n");
-    scanf("%d",&choix);
+    printf("4 - Objet transformable\n");
+    printf("5 - Matériau\n");
+    scanf("%d", &choix);
     switch(choix){
           case 1:deposer_objet(b->nourriture,j); break;
           case 2:deposer_objet(b->drogue,j); break;
           case 3:deposer_objet(b->arme,j); break;
-          case 4:deposer_objet(b->obj_construct,j); break;
+          case 4:deposer_objet(b->transformable,j); break;
+          case 5:deposer_objet(b->materiau,j); break;
           default:printf("Erreur, vous devez entrer un chiffre entre 1 et 4\n");
      }
 }
@@ -64,8 +74,9 @@ void retirer_objet_banque(banque_t * b,joueur_t * j){
           case 1:pt=b->nourriture; break;
           case 2:pt=b->drogue; break;
           case 3:pt=b->arme; break;
-          case 4:pt=b->obj_construct; break;
-          default:printf("Erreur, vous devez entrer un chiffre entre 1 et 4\n");
+          case 4:pt=b->transformable; break;
+          case 5:pt=b->materiau; break;
+          default:printf("Erreur, vous devez entrer un chiffre entre 1 et 5\n");
     }
     if(pt!=NULL){
         prendre_objet(pt,j);
