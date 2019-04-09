e81fb215fb7b9cbd145214e8dbe22ebd08105524
Doktity <38131269+Doktity@users.noreply.github.com>
2019-04-09 02:05:49 +0200

    Update maison.c

--- maison.c
+++ maison.c
@@ -1,48 +1,55 @@
 #include "maison.h"
-
-void maison(int choix, joueur_t * joueur, char * buffer){
-  char mem[512];
-
-	/* Traitement du choix de l'utilisateur */
-	switch(choix)
-	{
-		case 1: sprintf(buffer, "%s", ameliorer_maison(joueur));
-				break;
-		case 2: objet_afficher_liste(joueur->coffre, buffer);
-            strcpy(mem, buffer);
-            afficher_inventaire(joueur, buffer);
-            strcat(buffer, mem);
-				break;
-		case 3: utiliser_objet(joueur);
-				break;
-		case 4: deposer_objet(joueur->coffre, joueur); 
-				break;
-		case 5: prendre_objet(joueur->coffre, joueur);
-				break;
-	}
+void maison(joueur_t * joueur){
+  int choix;
+  system("clear");
+  do
+  /* Affichage du menu et saisie du choix */
+  {
+    printf("\nMaison\n------\nQue voulez-vous faire ?\n");
+    printf(" 1 - Améliorer maison\n");
+    printf(" 2 - Voir inventaire et coffre\n");
+    printf(" 3 - Utiliser objet\n");
+    printf(" 4 - Deposer objet\n");
+    printf(" 5 - Prendre objet\n");
+    printf(" 6 - Retour\n");
+    printf("Votre choix : ");
+    scanf("%d",&choix);
+    /* Traitement du choix de l'utilisateur */
+    switch(choix)
+    {
+      case 1: ameliorer_maison(joueur); break;
+      case 2: afficher_inventaire(joueur); objet_afficher_liste(joueur->coffre); break;
+      case 3: utiliser_objet(joueur); break;
+      case 4: deposer_objet(joueur->coffre, joueur); break;
+      case 5: prendre_objet(joueur->coffre, joueur);break;
+      case 6: break;
+      default: printf("Erreur: votre choix doit etre compris entre 1 et 6\n");
+    }
+  }
+  while(choix!=6);
 }
 
 
-char * ameliorer_maison(joueur_t * joueur){
+void ameliorer_maison(joueur_t * joueur){
   switch(joueur->maison)
   {
     case 1: if(joueur->pa >= 2){
               joueur->maison++;
               joueur->pa -= 2;
-              return "Amélioration réussie.";
+              printf("Amélioration réussie.\n");
             }else{
-              return "Pas assez de points d'actions ! (besoin de 2 pa)";
+              printf("Pas assez de points d'actions ! (besoin de 2 pa)\n");
             }
             break;
     case 2:
     case 3: if(joueur->pa == 6){
               joueur->maison++;
               joueur->pa -= 6;
-              return "Amélioration réussie.";
+              printf("Amélioration réussie.\n");
             }else{
-              return "Pas assez de points d'actions ! (besoin de 6 pa)";
+              printf("Pas assez de points d'actions ! (besoin de 6 pa)\n");
             }
             break;
-    default: return "Amélioration maximum de la maison atteint";
+    default: printf("Amélioration maximum de la maison atteint\n");
   }
 }
