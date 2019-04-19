3a4e3345d532eee64e45c1e9742e4f99f2931225
Younes <45875852+Buzzthespacekiwi@users.noreply.github.com>
2019-04-19 21:36:04 +0200

    Update chantier.h

--- chantier.h
+++ chantier.h
@@ -1,59 +1,59 @@
 #include "banque.h"
 
-typedef struct structure_s{
+typedef struct defense_s{
   char nom[TAILLE];
-  int defense; //nombre de défense que la structure générera
-  int pa_requis; //Nombre de points d'actions pour pouvoir construire la structure, on décrémente au fur et à mesure qu'un joueur se dépense au chantier
-  structure_t * structure_1; // Le premier structure requis pour pouvoir contruire la structure
-  structure_t * structure_2;// Le second structure requis pour pouvoir construire la structure
-  int nb_obj1;// Le nombre d'structures du type structure_1 nécessaire pour sa construction
-  int nb_obj2;// Le nombre d'structures du type structure_2 nécessaire pour sa construction
-  int construit;// défini si l'structure est déja construit ou non optionnel
-}structure_t;
+  int defense; //nombre de défense que la defense générera
+  int pa_requis; //Nombre de points d'actions pour pouvoir construire la defense, on décrémente au fur et à mesure qu'un joueur se dépense au chantier
+  objet_t * objet_1; // Le premier objet requis pour pouvoir contruire la defense
+  objet_t * objet_2;// Le second objet requis pour pouvoir construire la defense
+  int nb_obj1;// Le nombre d'objet du type objet_1 nécessaire pour sa construction
+  int nb_obj2;// Le nombre d'objet du type objet_2 nécessaire pour sa construction
+  int construit;// défini si la defense est déja construite ou non optionnel
+}defense_t;
 
 typedef struct partie_s{
-  strucutre_t val;
+  defense_t val;
   struct partie_s * pred;
   struct partie_s * succ;
 }partie_t;
 
-typedef struct liste_structure_s{
+typedef struct liste_defense_s{
   partie_t * ec;
   partie_t * drapeau;
-}liste_structure_t;
+}liste_defense_t;
 
 
 /**
 *Primitives d'accès à la liste
 */
-void structure_init_liste(liste_structure_t**);
+void defense_init_liste(liste_defense_t**);
 
-int structure_liste_vide(liste_structure_t*);
+int defense_liste_vide(liste_defense_t*);
 
-int structure_hors_liste(liste_structure_t*);
+int defense_hors_liste(liste_defense_t*);
 
-void structure_en_tete(liste_structure_t*);
+void defense_en_tete(liste_defense_t*);
 
-void structure_en_queue(liste_structure_t*);
+void defense_en_queue(liste_defense_t*);
 
-void structure_precedent(liste_structure_t*);
+void defense_precedent(liste_defense_t*);
 
-void structure_suivant(liste_structure_t*);
+void defense_suivant(liste_defense_t*);
 
-void structure_valeur_elt(structure_t*,liste_structure_t*);
+void defense_valeur_elt(defense_t*,liste_defense_t*);
 
-void structure_modif_elt(structure_t,liste_structure_t*);
+void defense_modif_elt(defense_t,liste_defense_t*);
 
-void structure_oter_elt(liste_structure_t*);
+void defense_oter_elt(liste_defense_t*);
 
-void structure_ajout_droit(structure_t,liste_structure_t*);
+void defense_ajout_droit(defense_t,liste_defense_t*);
 
-void structure_ajout_gauche(structure_t,liste_structure_t*);
+void defense_ajout_gauche(defense_t,liste_defense_t*);
 
-void chantier(liste_structure_t *, banque_t *, joueur_t *);
+void chantier(liste_defense_t *, banque_t *, joueur_t *);
 
-void structure_afficher_liste(liste_structure_liste *);
+void defense_afficher_liste(liste_defense_liste *);
 
-int structure_dispo(structure_t,banque_t *);
+int defense_dispo(defense_t,banque_t *);
 
-int joueur_travailler(joueur_t *, liste_structure_t *, banque_t *);
+int joueur_travailler(joueur_t *, liste_defense_t *, banque_t *);
